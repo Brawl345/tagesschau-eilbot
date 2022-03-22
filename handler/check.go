77132c3d4864cca4a5c465a1d66f2b510ae28fd7
Andreas Bielawski <brawl@nyanya.de>
2022-03-22 10:05:20 +0100

    Fix nil pointer on flood error

--- handler/check.go
+++ handler/check.go
@@ -134,7 +134,6 @@ func (h Handler) sendText(subscriber int64, text string, sendOptions *telebot.Se
 	_, err := h.Bot.Send(telebot.ChatID(subscriber), text, sendOptions)
 
 	var telebotError *telebot.Error
-	var floodError *telebot.FloodError
 
 	if err != nil {
 		if errors.Is(err, telebot.ErrChatNotFound) {
@@ -146,8 +145,8 @@ func (h Handler) sendText(subscriber int64, text string, sendOptions *telebot.Se
 			h.DB.Subscribers.Delete(subscriber)
 			h.DB.Subscribers.Create(migratedTo)
 			return h.sendText(migratedTo, text, sendOptions)
-		} else if errors.As(err, floodError) {
-			retryAfter := floodError.RetryAfter
+		} else if errors.As(err, &telebot.FloodError{}) {
+			retryAfter := err.(telebot.FloodError).RetryAfter
 			log.Printf("%d: Flood error, retrying after: %d seconds", subscriber, retryAfter)
 			time.Sleep(time.Duration(retryAfter) * time.Second)
 			h.sendText(subscriber, text, sendOptions)
